676653a0f7db65d596af612bf838c1d5ecd7c6d4
daixi <xi.dai1990@gmail.com>
2014-06-21 00:01:53 +0800

    fix bugs and add testing data.

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -21,35 +21,30 @@ public class Percolation {
         checkBound(i, j);
         int site = toOneDimension(i, j);
         openSites[site] = true;
-        if (isOpen(i, j - 1)){ //left
+        if (!isOutOfBoundary(i, j - 1) && isOpen(i, j - 1)){ //left
             uf.union(site, toOneDimension(i, j - 1));
         }
-        if (isOpen(i - 1, j)){ //up
+        if (!isOutOfBoundary(i - 1, j) && isOpen(i - 1, j)){ //up
             uf.union(site, toOneDimension(i - 1, j));
         }
-        if (isOpen(i, j + 1)){ //right
+        if (!isOutOfBoundary(i, j + 1) && isOpen(i, j + 1)){ //right
             uf.union(site, toOneDimension(i, j + 1));
         }
-        if (isOpen(i + 1, j)){ //down
+        if (!isOutOfBoundary(i + 1, j) && isOpen(i + 1, j)){ //down
             uf.union(site, toOneDimension(i + 1, j));
         }
     }         // open site (row i, column j) if it is not already
-
-    private int toOneDimension(int i, int j) {
-        checkBound(i, j);
-        return (i - 1) * N - (j - 1);
-    }
 
     public boolean isOpen(int i, int j){
         checkBound(i, j);
         return openSites[toOneDimension(i, j)];
     }    // is site (row i, column j) open?
-    
+
     public boolean isFull(int i, int j) {
         checkBound(i, j);
         return uf.connected(toOneDimension(i, j), virtualNode);
     }   // is site (row i, column j) full?
-    
+
     public boolean percolates(){
         //TODO is percolates?
         for (int i = N*N - 1; i > N*N - N - 1; i--){
@@ -58,6 +53,14 @@ public class Percolation {
             }
         }
         return false;
+    }
+
+    private int toOneDimension(int i, int j) {
+        return (i - 1) * N + (j - 1);
+    }
+
+    private boolean isOutOfBoundary(int i, int j) {
+        return (i <= 0 || i > N || j <= 0 || j > N);
     }
 
     private void checkBound(int i, int j) {
