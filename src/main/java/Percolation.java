12283315d908ce0e50f6e44d9790746a005167f8
daixi <xi.dai1990@gmail.com>
2014-06-22 23:32:52 +0800

    percolationstats

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,41 +1,44 @@
 public class Percolation {
     private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF bottomUF;
     private int N;
     private int virtualNode;
     private boolean[] openSites;
 
-    public Percolation(int N){
+    public Percolation(int N) {
+        if (N <= 0) throw new IllegalArgumentException("N is out of boundary!");
         virtualNode = N * N;
         uf = new WeightedQuickUnionUF(N * N + 1); //uf.id[N * N] is the virtual top node.
+        uf = new WeightedQuickUnionUF(N);
         this.N = N;
         openSites = new boolean[N * N];
-        for (int i = 0; i < N * N; i++){
+        for (int i = 0; i < N * N; i++) {
             openSites[i] = false;
         }
     }              // create N-by-N grid, with all sites blocked
-    public void open(int i, int j){
+
+    public void open(int i, int j) {
         checkBound(i, j);
         int site = toOneDimension(i, j);
         openSites[site] = true;
-        if (isTopNode(i, j)){
+        if (isTopNode(i, j)) {
             uf.union(site, virtualNode);
         }
-        if (!isOutOfBoundary(i, j - 1) && isOpen(i, j - 1)){ //left
+        if (!isOutOfBoundary(i, j - 1) && isOpen(i, j - 1)) { //left
             uf.union(site, toOneDimension(i, j - 1));
         }
-        if (!isOutOfBoundary(i - 1, j) && isOpen(i - 1, j)){ //up
+        if (!isOutOfBoundary(i - 1, j) && isOpen(i - 1, j)) { //up
             uf.union(site, toOneDimension(i - 1, j));
         }
-        if (!isOutOfBoundary(i, j + 1) && isOpen(i, j + 1)){ //right
+        if (!isOutOfBoundary(i, j + 1) && isOpen(i, j + 1)) { //right
             uf.union(site, toOneDimension(i, j + 1));
         }
-        if (!isOutOfBoundary(i + 1, j) && isOpen(i + 1, j)){ //down
+        if (!isOutOfBoundary(i + 1, j) && isOpen(i + 1, j)) { //down
             uf.union(site, toOneDimension(i + 1, j));
         }
     }         // open site (row i, column j) if it is not already
 
-
-    public boolean isOpen(int i, int j){
+    public boolean isOpen(int i, int j) {
         checkBound(i, j);
         return openSites[toOneDimension(i, j)];
     }    // is site (row i, column j) open?
@@ -45,13 +48,16 @@ public class Percolation {
         return uf.connected(toOneDimension(i, j), virtualNode);
     }   // is site (row i, column j) full?
 
-    public boolean percolates(){
-        for (int i = N*N - 1; i > N*N - N - 1; i--){
-            if (uf.connected(i, virtualNode)){
-                return true;
-            }
+    public boolean percolates() {
+//        for (int i = N * N - 1; i > N * N - N - 1; i--) {
+//            if (uf.connected(i, virtualNode)) {
+//                return true;
+//            }
+//        }
+//        return false;
+        while (true){
+
         }
-        return false;
     }
 
     private boolean isTopNode(int i, int j) {
--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -1,2 +1,54 @@
 public class PercolationStats {
+    private double[] probabilities;
+    private int t;
+
+    public PercolationStats(int N, int T) {
+        t = T;
+        checkBoundary(N, T);
+        probabilities = new double[T];
+        for (int i = 0; i < T; i++) {
+            double count = 0;
+            Percolation percolation = new Percolation(N);
+            while (!percolation.percolates()) {
+                int x = StdRandom.uniform(1, N + 1);
+                int y = StdRandom.uniform(1, N + 1);
+                if (!percolation.isOpen(x, y)) {
+                    percolation.open(x, y);
+                    count++;
+                }
+            }
+            probabilities[i] = count / (N * N);
+        }
+    }    // perform T independent computational experiments on an N-by-N grid
+
+    private void checkBoundary(int n, int t) {
+        if (n <= 0 || t <= 0) throw new IllegalArgumentException("Illegal Argument!");
+    }
+
+    public double mean() {
+        return StdStats.mean(probabilities);
+    }                    // sample mean of percolation threshold
+
+    public double stddev() {
+        return StdStats.stddev(probabilities);
+    }                  // sample standard deviation of percolation threshold
+
+    public double confidenceLo() {
+        return mean() - 1.96 * StdStats.stddevp(probabilities) / Math.sqrt(t);
+    }            // returns lower bound of the 95% confidence interval
+
+    public double confidenceHi() {
+        return mean() + 1.96 * StdStats.stddevp(probabilities) / Math.sqrt(t);
+    }           // returns upper bound of the 95% confidence interval
+
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(N, T);
+
+        System.out.println("mean                    = " + ps.mean());
+        System.out.println("stddev                  = " + ps.stddev());
+        System.out.println("95% confidence interval = " + ps.confidenceLo() + ", " + ps.confidenceHi());
+    }  // test client, described below
+
 }
