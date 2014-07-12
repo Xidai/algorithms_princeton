910b3d61d50f4d0009faaa8e8c469414042eee07
daixi <xi.dai1990@gmail.com>
2014-07-12 16:07:05 +0800

    subset for assignment 2

--- src/main/java/Subset.java
+++ src/main/java/Subset.java
@@ -1,5 +1,17 @@
+import java.util.Iterator;
+
 public class Subset {
     public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<String>();
+        String[] input = StdIn.readAllStrings();
+        for (String anInput : input) {
+            randomizedQueue.enqueue(anInput);
+        }
 
+        Iterator<String> iterator = randomizedQueue.iterator();
+        for (int i = 0; i < k; i++){
+            System.out.println(iterator.next());
+        }
     }
 }
